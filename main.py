2b378508e6398d78f0e17bdb6ff75dc3da7d98d5
xpecel <ifanpujiyanto172@gmail.com>
2023-08-21 15:08:38 +0700

    Fix: Perbaikan pada dependency dan sedikit peningkatan pada kode

--- main.py
+++ main.py
@@ -1,75 +1,76 @@
 import os
+import sys
 import requests
 from bs4 import BeautifulSoup
 from colorama import Fore, init
 
-init()
-print(Fore.LIGHTCYAN_EX + """
-╔═══════════════════════════════════╗
-║      github.com/CallMeDimas/      ║
-╟───────────────────────────────────╢
-║  ╦╔═╔═╗    ╔═╗╔═╗╦═╗╔═╗╔═╗╔═╗╦═╗  ║
-║  ╠╩╗║      ╚═╗║  ╠╦╝╠═╣╠═╝║╣ ╠╦╝  ║
-║  ╩ ╩╚═╝────╚═╝╚═╝╩╚═╩ ╩╩  ╚═╝╩╚═  ║
-╟───────────────────────────────────╢
-║       KOMIKCAST.VIP SCRAPER       ║
-╚═══════════════════════════════════╝
-""")
-print(Fore.WHITE + "Name: KomikCast Scraper")
-print(Fore.WHITE + "Author: CallMeDimas")
-print(Fore.WHITE + "Github: https://github.com/CallMeDimas/")
-print(Fore.WHITE + "Contact: callmedimas@proton.me")
-print(Fore.WHITE + "Version: v1.0.0")
+class KomikCast:
+    def __init__(self):
+        print(Fore.LIGHTCYAN_EX + """
+        ╔═══════════════════════════════════╗
+        ║      github.com/CallMeDimas/      ║
+        ╟───────────────────────────────────╢
+        ║  ╦╔═╔═╗    ╔═╗╔═╗╦═╗╔═╗╔═╗╔═╗╦═╗  ║
+        ║  ╠╩╗║      ╚═╗║  ╠╦╝╠═╣╠═╝║╣ ╠╦╝  ║
+        ║  ╩ ╩╚═╝────╚═╝╚═╝╩╚═╩ ╩╩  ╚═╝╩╚═  ║
+        ╟───────────────────────────────────╢
+        ║       KOMIKCAST.VIP SCRAPER       ║
+        ╚═══════════════════════════════════╝
+        """)
+        print(Fore.WHITE + "Name: KomikCast Scraper")
+        print(Fore.WHITE + "Author: CallMeDimas")
+        print(Fore.WHITE + "Github: https://github.com/CallMeDimas/")
+        print(Fore.WHITE + "Contact: callmedimas@proton.me")
+        print(Fore.WHITE + "Version: v1.0.0")
 
+    def _start_requests(self, url):
+        meta_data = {}
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        }
 
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        contents = soup.select('div#content')
+        meta_data['title'] = contents[0].find('h1').text
+        raw_img = contents[0].find(class_='main-reading-area').find_all('img')
+        meta_data['img_srcs'] = [img.get('src') for img in raw_img]
+        return meta_data
 
-def scrape_manga_chapter(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-    }
+    def _creating_directory(self, sub_folder):
+        directory_name = f'downloads/{sub_folder}/'
+        if not os.path.exists(directory_name):
+            os.makedirs(directory_name)
+        else:
+            return 'Directory exists'
+        return directory_name
 
-    response = requests.get(url, headers=headers)
+    def download_images(self, url):
+        meta_data = self._start_requests(url)
+        directory_name = self._creating_directory(meta_data['title'])
 
-    soup = BeautifulSoup(response.text, 'html.parser')
+        if directory_name == 'Directory exists':
+            print(f'\n{Fore.GREEN}Chapter already downloaded!\n')
+            sys.exit(0)
 
-    div_element1 = soup.find(class_="chapter_headpost")
-    h1_element = div_element1.find('h1')
-    title = h1_element.get_text()
+        counter = 1
+        print(f'\nManga title: {meta_data["title"]}')
+        for src in meta_data['img_srcs']:
+            response = requests.get(src, stream=True)
+            with open(os.path.join(directory_name, f'{counter}.jpg'), 'wb') as out_file:
+                out_file.write(response.content)
+            print(f'Successfully download page number {counter}!')
+            counter += 1
 
-    div_element = soup.find(class_="main-reading-area")
+        return counter
 
-    img_elements = div_element.find_all('img')
-
-    img_srcs = [img.get('src') for img in img_elements]
-
-    return title, img_srcs
-
-def download_images(img_srcs, directory='images'):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    count = 0
-    for i, src in enumerate(img_srcs):
-        response = requests.get(src, stream=True)
-        with open(os.path.join(directory, 'image{}.jpg'.format(i)), 'wb') as out_file:
-            out_file.write(response.content)
-        print('Downloaded... image{}'.format(i))
-        count += 1
-
-    return count
 
 def main():
     url = input('\nEnter the Chapter Link: ')
-
     os.system('cls' if os.name == 'nt' else 'clear')
-
-    title, img_srcs = scrape_manga_chapter(url)
-    
-    print(f'Manga Title: {title}\n')
-    
-    count = download_images(img_srcs)
-
-    print(f'\n{Fore.GREEN}Successfully Downloaded: {count} images\n')
+    manga = KomikCast()
+    count = manga.download_images(url)
+    print(f'\n{Fore.GREEN}Successfully download {count} images!\n')
 
 if __name__ == "__main__":
     main()
